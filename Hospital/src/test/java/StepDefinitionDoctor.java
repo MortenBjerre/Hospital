ac6e816017f0189990ff9b0370e5e13c528f86fc
MortenBjerre <48279971+MortenBjerre@users.noreply.github.com>
2019-04-05 14:57:29 +0200

    Added cucumber tests for nurse/doctor scenario

---
+++ Hospital/src/test/java/StepDefinitionDoctor.java
@@ -0,0 +1,9 @@
+import Hospital.Doctor;
+import cucumber.api.java.en.Given;
+
+public class StepDefinitionDoctor {
+	@Given("^That I am a doctor$")
+	public void that_I_am_a_doctor() {
+		Doctor doctor = new Doctor();
+	}
+}
---
+++ Hospital/src/test/java/StepDefinitionNurse.java
@@ -0,0 +1,16 @@
+import Hospital.Nurse;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class StepDefinitionNurse {
+	@Given("^That I am a nurse$")
+	public void that_I_am_a_nurse() {
+		Nurse nurse = new Nurse();
+	}
+
+	@Then("^edit the patients heatlh data$")
+	public void edit_the_patients_heatlh_data() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
