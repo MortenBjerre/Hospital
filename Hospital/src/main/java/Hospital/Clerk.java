09baf8941cccabba795d84ccfdd30064cacab142
erick <erickastljensen@gmail.com>
2019-04-10 13:25:36 +0200

    added documentation to clerk

--- Hospital/src/main/java/Hospital/Clerk.java
+++ Hospital/src/main/java/Hospital/Clerk.java
@@ -4,11 +4,23 @@ import java.util.Date;
 
 public class Clerk extends Staff {
 	
+	/**
+	 * Constructor for clerk. Sets staff information
+	 * @param email
+	 * @param name
+	 * @param surname
+	 * @param birthday
+	 * @param gender
+	 * @param department
+	 */
 	public Clerk(String email, String name, String surname, Date birthday, String gender,
 			String department) {
 		super(email, name, surname, birthday, gender, department);	
 	}
 	
+	/**
+	 * Checks if this staff member has editing permission to a register
+	 */
 	public boolean hasWriteAccessTo(Register r) {
 		if (r instanceof PatientRegister) {
 			return true;
