c0d4bd804a73a47797924887b6be24e1310d0630
erick <erickastljensen@gmail.com>
2019-04-10 13:22:49 +0200

    made new branch

--- Hospital/src/main/java/Hospital/Clerk.java
+++ Hospital/src/main/java/Hospital/Clerk.java
@@ -3,7 +3,7 @@ package Hospital;
 import java.util.Date;
 
 public class Clerk extends Staff {
-
+	
 	public Clerk(String email, String name, String surname, Date birthday, String gender,
 			String department) {
 		super(email, name, surname, birthday, gender, department);	
@@ -13,7 +13,7 @@ public class Clerk extends Staff {
 		if (r instanceof PatientRegister) {
 			return true;
 		}
-		return false;
+		return false; 
 	}
 }
 
