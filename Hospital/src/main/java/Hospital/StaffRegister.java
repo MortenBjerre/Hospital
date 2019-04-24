54f8bbfd1b8bf30f215e3455cc925568e881eb6c
MortenBjerre <48279971+MortenBjerre@users.noreply.github.com>
2019-04-24 12:38:45 +0200

    search for role deleted

--- Hospital/src/main/java/Hospital/StaffRegister.java
+++ Hospital/src/main/java/Hospital/StaffRegister.java
@@ -83,34 +83,4 @@ public class StaffRegister extends Register<Staff> {
 		users.put(clerk.getSerialnum(), clerk);
 		return clerk.getSerialnum();
 	}
-
-	
-//	/**
-//	 * Searches for a Staff with a matching Role
-//	 * @param role
-//	 * @return
-//	 */
-//	public String[] searchRole(String role) {
-//		ArrayList<Staff> matches = findRole(role);
-//		String[] m = new String[matches.size()];
-//		for (int i = 0; i < matches.size(); i++) {
-//			m[i] = matches.get(i).toString();
-//		}
-//		return m;
-//	}
-//	
-//	/**
-//	 * Searches for a Staff with a matching Role
-//	 * @param role
-//	 * @return
-//	 */
-//	protected ArrayList<Staff> findRole(String role) {
-//		ArrayList<Staff> matches = new ArrayList<Staff>();
-//		for (Integer key : users.keySet()) {
-//			if (users.get(key).getRole() == role) {
-//				matches.add(users.get(key));
-//			}
-//		}
-//		return matches;
-//	}
 }
