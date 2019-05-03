b14163171524a2c58797b91dcb3f66dfb0fa0fb2
erick <erickastljensen@gmail.com>
2019-05-03 15:02:40 +0200

    can get coverage, run mavent test on pom

--- Hospital/src/test/java/jUnitTesting/Test.java
+++ Hospital/src/test/java/jUnitTesting/Test.java
@@ -1,6 +1,14 @@
 package jUnitTesting;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/main/resources",
+        glue = "stepDefinition"
+)
 
 public class Test {
 
