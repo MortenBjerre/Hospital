18619b4413793fa8bbc985051499d606f278f389
Morten Bjerre <Morten Bjerre@MortensPC>
2019-05-04 13:01:54 +0200

    removed unused rows

--- Hospital/src/main/java/GUI/RegisterPatient.java
+++ Hospital/src/main/java/GUI/RegisterPatient.java
@@ -62,9 +62,9 @@ public class RegisterPatient extends JFrame {
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 89, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JLabel lblName = new JLabel("Name: ");
@@ -72,7 +72,7 @@ public class RegisterPatient extends JFrame {
 		gbc_lblName.anchor = GridBagConstraints.EAST;
 		gbc_lblName.insets = new Insets(0, 0, 5, 5);
 		gbc_lblName.gridx = 2;
-		gbc_lblName.gridy = 3;
+		gbc_lblName.gridy = 1;
 		contentPane.add(lblName, gbc_lblName);
 		
 		Name = new JTextField();
@@ -80,7 +80,7 @@ public class RegisterPatient extends JFrame {
 		gbc_txtHealthDataInput.insets = new Insets(0, 0, 5, 5);
 		gbc_txtHealthDataInput.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtHealthDataInput.gridx = 3;
-		gbc_txtHealthDataInput.gridy = 3;
+		gbc_txtHealthDataInput.gridy = 1;
 		contentPane.add(Name, gbc_txtHealthDataInput);
 		Name.setColumns(10);
 		
@@ -89,7 +89,7 @@ public class RegisterPatient extends JFrame {
 		gbc_lblSurname.anchor = GridBagConstraints.EAST;
 		gbc_lblSurname.insets = new Insets(0, 0, 5, 5);
 		gbc_lblSurname.gridx = 2;
-		gbc_lblSurname.gridy = 4;
+		gbc_lblSurname.gridy = 2;
 		contentPane.add(lblSurname, gbc_lblSurname);
 		
 		Surname = new JTextField();
@@ -97,7 +97,7 @@ public class RegisterPatient extends JFrame {
 		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_1.gridx = 3;
-		gbc_textField_1.gridy = 4;
+		gbc_textField_1.gridy = 2;
 		contentPane.add(Surname, gbc_textField_1);
 		Surname.setColumns(10);
 		
@@ -106,7 +106,7 @@ public class RegisterPatient extends JFrame {
 		gbc_lblAddress.anchor = GridBagConstraints.EAST;
 		gbc_lblAddress.insets = new Insets(0, 0, 5, 5);
 		gbc_lblAddress.gridx = 2;
-		gbc_lblAddress.gridy = 5;
+		gbc_lblAddress.gridy = 3;
 		contentPane.add(lblAddress, gbc_lblAddress);
 		
 		Address = new JTextField();
@@ -115,7 +115,7 @@ public class RegisterPatient extends JFrame {
 		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_2.gridx = 3;
-		gbc_textField_2.gridy = 5;
+		gbc_textField_2.gridy = 3;
 		contentPane.add(Address, gbc_textField_2);
 		
 		JLabel lblEmail = new JLabel("Email: ");
@@ -123,7 +123,7 @@ public class RegisterPatient extends JFrame {
 		gbc_lblEmail.anchor = GridBagConstraints.EAST;
 		gbc_lblEmail.insets = new Insets(0, 0, 5, 5);
 		gbc_lblEmail.gridx = 2;
-		gbc_lblEmail.gridy = 6;
+		gbc_lblEmail.gridy = 4;
 		contentPane.add(lblEmail, gbc_lblEmail);
 		
 		Email = new JTextField();
@@ -132,7 +132,7 @@ public class RegisterPatient extends JFrame {
 		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_3.gridx = 3;
-		gbc_textField_3.gridy = 6;
+		gbc_textField_3.gridy = 4;
 		contentPane.add(Email, gbc_textField_3);
 		
 		JLabel lblBirthday = new JLabel("Birthday: ");
@@ -140,7 +140,7 @@ public class RegisterPatient extends JFrame {
 		gbc_lblBirthday.anchor = GridBagConstraints.EAST;
 		gbc_lblBirthday.insets = new Insets(0, 0, 5, 5);
 		gbc_lblBirthday.gridx = 2;
-		gbc_lblBirthday.gridy = 7;
+		gbc_lblBirthday.gridy = 5;
 		contentPane.add(lblBirthday, gbc_lblBirthday);
 		
 		JDateChooser dateChooser = new JDateChooser();
@@ -155,7 +155,7 @@ public class RegisterPatient extends JFrame {
 		gbc_dateChooser.insets = new Insets(0, 0, 5, 5);
 		gbc_dateChooser.fill = GridBagConstraints.BOTH;
 		gbc_dateChooser.gridx = 3;
-		gbc_dateChooser.gridy = 7;
+		gbc_dateChooser.gridy = 5;
 		contentPane.add(dateChooser, gbc_dateChooser);
 		
 //		textField_4 = new JTextField();
@@ -172,7 +172,7 @@ public class RegisterPatient extends JFrame {
 		gbc_lblGender.anchor = GridBagConstraints.EAST;
 		gbc_lblGender.insets = new Insets(0, 0, 5, 5);
 		gbc_lblGender.gridx = 2;
-		gbc_lblGender.gridy = 8;
+		gbc_lblGender.gridy = 6;
 		contentPane.add(lblGender, gbc_lblGender);
 		
 		Gender = new JTextField();
@@ -181,7 +181,7 @@ public class RegisterPatient extends JFrame {
 		gbc_textField_5.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_5.gridx = 3;
-		gbc_textField_5.gridy = 8;
+		gbc_textField_5.gridy = 6;
 		contentPane.add(Gender, gbc_textField_5);
 		
 		JLabel lblPhoneNumber = new JLabel("Phone Number: ");
@@ -189,7 +189,7 @@ public class RegisterPatient extends JFrame {
 		gbc_lblPhoneNumber.anchor = GridBagConstraints.EAST;
 		gbc_lblPhoneNumber.insets = new Insets(0, 0, 5, 5);
 		gbc_lblPhoneNumber.gridx = 2;
-		gbc_lblPhoneNumber.gridy = 9;
+		gbc_lblPhoneNumber.gridy = 7;
 		contentPane.add(lblPhoneNumber, gbc_lblPhoneNumber);
 		
 		Number = new JTextField();
@@ -198,7 +198,7 @@ public class RegisterPatient extends JFrame {
 		gbc_textField_6.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_6.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_6.gridx = 3;
-		gbc_textField_6.gridy = 9;
+		gbc_textField_6.gridy = 7;
 		contentPane.add(Number, gbc_textField_6);
 		
 		JLabel lblAlive = new JLabel("Alive: ");
@@ -207,7 +207,7 @@ public class RegisterPatient extends JFrame {
 		gbc_lblAlive.insets = new Insets(0, 0, 5, 5);
 		gbc_lblAlive.anchor = GridBagConstraints.EAST;
 		gbc_lblAlive.gridx = 2;
-		gbc_lblAlive.gridy = 10;
+		gbc_lblAlive.gridy = 8;
 		contentPane.add(lblAlive, gbc_lblAlive);
 		
 		JButton btnRegister = new JButton("Register");
@@ -254,7 +254,7 @@ public class RegisterPatient extends JFrame {
 		gbc_list.insets = new Insets(0, 0, 5, 5);
 		gbc_list.fill = GridBagConstraints.BOTH;
 		gbc_list.gridx = 3;
-		gbc_list.gridy = 10;
+		gbc_list.gridy = 8;
 		contentPane.add(list, gbc_list);
 		
 		JLabel lblHealthData = new JLabel("Health Data:");
@@ -262,7 +262,7 @@ public class RegisterPatient extends JFrame {
 		gbc_lblHealthData.anchor = GridBagConstraints.EAST;
 		gbc_lblHealthData.insets = new Insets(0, 0, 5, 5);
 		gbc_lblHealthData.gridx = 2;
-		gbc_lblHealthData.gridy = 11;
+		gbc_lblHealthData.gridy = 9;
 		contentPane.add(lblHealthData, gbc_lblHealthData);
 		
 		txtHealthDataInput = new JTextField();
@@ -271,13 +271,13 @@ public class RegisterPatient extends JFrame {
 		gbc_txtHealthDataInput1.insets = new Insets(0, 0, 5, 5);
 		gbc_txtHealthDataInput1.fill = GridBagConstraints.BOTH;
 		gbc_txtHealthDataInput1.gridx = 3;
-		gbc_txtHealthDataInput1.gridy = 11;
+		gbc_txtHealthDataInput1.gridy = 9;
 		contentPane.add(txtHealthDataInput, gbc_txtHealthDataInput1);
 		txtHealthDataInput.setColumns(10);
 		GridBagConstraints gbc_btnRegister = new GridBagConstraints();
 		gbc_btnRegister.insets = new Insets(0, 0, 0, 5);
 		gbc_btnRegister.gridx = 3;
-		gbc_btnRegister.gridy = 12;
+		gbc_btnRegister.gridy = 10;
 		contentPane.add(btnRegister, gbc_btnRegister);
 		
 		
